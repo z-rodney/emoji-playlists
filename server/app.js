82decfe939066013f5573ecb38ce849fabd11c14
Zaina Rodney <zainarodney@gmail.com>
2021-03-14 22:17:15 -0400

    cleaned up server code

--- server/app.js
+++ server/app.js
@@ -13,38 +13,26 @@ app.get('*', (req, res) => {
 })
 
 app.get('/auth_success', async (req, res, next) => {
-  const { code: accessCode, error } = req.query
-  if (error) {
-    res.send('uhoh - access denied')
-  } else {
-    try {
-      const authResponse = await axios({
-        method: 'post',
-        url: 'https://accounts.spotify.com/api/token',
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-        }, data: qs.stringify({
-          grant_type: 'authorization_code',
-          code: accessCode,
-          redirect_uri: process.env.REDIRECT_URI,
-          client_id: process.env.SPOTIFY_CLIENT_ID,
-          client_secret: process.env.SPOTIFY_CLIENT_SECRET
-        })
+  const { code: accessCode } = req.body
+  try {
+    const authResponse = await axios({
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+      }, data: qs.stringify({
+        grant_type: 'authorization_code',
+        code: accessCode,
+        redirect_uri: process.env.REDIRECT_URI,
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        client_secret: process.env.SPOTIFY_CLIENT_SECRET
       })
-      console.log(authResponse.data)
-      res.send('congrats! spotify is authorized')
-    } catch (err) {
-      console.log(err)
-    }
+    })
+    console.log(authResponse.data)
+    res.send('congrats! spotify is authorized')
+  } catch (err) {
+    console.log(err)
   }
-})
-
-app.get('/authorize', async (req, res, next) => {
-  /*TO-DO: add access scopes */
-  const scopes = qs.stringify({
-    scope: 'user-read-recently-played user-top-read playlist-modify-public'
-  })
-  res.redirect(`https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REDIRECT_URI}&${scopes}`)
 })
 
 const PORT = 8082;
